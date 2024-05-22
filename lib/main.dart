81f5e9d8b33bfab518163109fee28b4f9f0ff172
faithtnuc <fatihtunc.dev@gmail.com>
2024-05-22 06:54:04 +0300

    Cloud functions has been added.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 import 'package:snapncook/providers/detector_provider.dart';
+import 'package:snapncook/views/detected_ingredients_view.dart';
 import 'package:snapncook/views/detector_view.dart';
 
 import 'firebase_options.dart';
@@ -15,6 +17,23 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  try {
+    final userCredential =
+    await FirebaseAuth.instance.signInAnonymously();
+    //print("Signed in with temporary account.");
+  } on FirebaseAuthException catch (e) {
+    switch (e.code) {
+      case "operation-not-allowed":
+        //print("Anonymous auth hasn't been enabled for this project.");
+        throw Exception("Anonymous auth hasn't been enabled for this project.");
+        break;
+      default:
+        //print("Unknown error.");
+        throw Exception("Unknown error");
+    }
+  }
+
   runApp(const MyApp());
 }
 
@@ -36,7 +55,7 @@ class MyApp extends StatelessWidget {
         builder: (context, orientation, screenType) {
           return MaterialApp(
             debugShowCheckedModeBanner: false,
-            title: 'Flutter Demo',
+            title: 'Snap & Cook',
             theme: ThemeData(
               colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
               useMaterial3: true,
